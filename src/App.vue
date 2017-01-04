001dc36617f096583216e8001ba2878fdb0a619f
highmind <804798382@qq.com>
2017-01-04 17:08:37 +0800

    vue-test

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <btn></btn>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Btn from './components/Btn'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Btn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
