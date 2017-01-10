8a906321918bbdced4ffd212dfd689f1a88b8311
highmind <804798382@qq.com>
2017-01-10 11:26:36 +0800

    add

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
     <button class="button"
-    v-bind:class="{ 'danger': isDanger }"
-    v-on:click="dothings()">{{text}}</button>
-
+    v-bind:class="classObject"
+    v-on:click="dothings">{{text}}</button>
 </template>
 
 <script>
@@ -15,20 +13,30 @@ export default {
   },
   data () {
     return {
-      // text: '默认按钮'
+      btnCls: "default"
     }
+  },
+  computed:{
+    classObject(){
+      return(
+        this.type
+      )
+    }
+
   },
   methods: {
     dothings(){
-      console.log(111)
+      console.log(this.type)
+      this.isDanger = !this.isDanger
     }
-
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.button{display: block;border:none;width:80px;height:40px;border-radius:4px;border:none;cursor:pointer}
-.default-btn{background-color:#f6f8fa;color:#656B79; }
+.danger{background-color: #ef4f4f;color:#fff;}
+.button{display: block;border:none;width:80px;outline: none;height:40px;border-radius:4px;border:none;cursor:pointer}
+.default{background-color:#f6f8fa;color:#656B79; }
+.primary{background-color: #26a2ff;color:#fff;}
 </style>
