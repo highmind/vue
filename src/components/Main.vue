70d125fb199b2cf75f3346ff15b3b72b85715957
2de_100006143904_highmind <2de_100006143904_highmind@git.cloud.tencent.com>
2018-07-25 17:48:01 +0800

    solt

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="content">
+    <NavBar>
+      <span slot="navbar-left">返回</span>
+      <span slot="navbar-center">开眼视频</span>
+      <span slot="navbar-right">查看更多</span>
+    </NavBar>
     <Child>
       <h2 slot="header">念奴娇 赤壁怀古</h2>
       <p>大江东去，浪淘尽</p>
@@ -11,6 +16,7 @@
 <script>
  // slot插槽的父组件
 import Child from '@/components/Child'
+import NavBar from '@/components/NavBar'
 
 export default {
   name: 'main',
@@ -23,6 +29,10 @@ export default {
   },
   methods: {
    
+  },
+  components: {
+    Child,
+    NavBar
   }
 }
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-left">
+    	<slot name="navbar-left"></slot>
+    </div>
+    <div class="navbar-center">
+    	<slot name="navbar-center"></slot>
+    </div>
+    <div class="navbar-right">
+    	<slot name="navbar-right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import child1 from './Child1'
+export default {
+  name: 'navbar',
+  data () {
+    return {
+     
+    }
+  },
+  mounted:function(){
+  	
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+  .navbar{width:400px;height:40px;line-height:40px;display:flex;background-color:orange;color:#fff;}
+  .navbar-left{flex:1;}
+  .navbar-center{flex:3;}
+  .navbar-right{flex:1;}
+</style>
